351bb261de07e0f530742bc9e2a51338b59fc862
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-01-27 13:31:18 +0100

    Add s3 backend configuration

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "ec2-instance-roadmap"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
